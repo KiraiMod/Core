202ffe11c69c7762162917a3056708f5488dc758
xKiraiChan <xKiraiChan@gmail.com>
2022-03-12 23:03:54 -0500

    add vrc motion state, remove debug log

---
+++ Types/VRCMotionState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TypeScanner.Types;
+using UnityEngine;
+
+namespace KiraiMod.Core.Types
+{
+    public static class VRCMotionState
+    {
+        public static Type Type = ClassDef.Create("VRCMotionState")
+            .FromAssembly(Events.Hooks.AssemblyCSharp)
+            .DerivesFrom<MonoBehaviour>()
+            .WithMethods(
+                MethodDef.Create().WithName("Awake"),
+                MethodDef.Create().WithName("Reset"),
+                MethodDef.Create().WithName("Start"),
+                MethodDef.Create().WithName("OnControllerColliderHit")
+            )
+            .Setup()
+            .Resolved;
+
+        static VRCMotionState() => Type.LogAs("VRCMotionState");
+    }
+}
