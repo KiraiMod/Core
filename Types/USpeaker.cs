bad252f5cddcfa62badecf35f3391d0c0247c84f
xKiraiChan <xKiraiChan@gmail.com>
2022-03-14 23:05:35 -0400

    add uspeaker, loudmic

---
+++ Types/USpeaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TypeScanner.Types;
+using UnityEngine;
+
+namespace KiraiMod.Core.Types
+{
+    public static class USpeaker
+    {
+        // this looks like a really fragile signature
+        public static Type Type = ClassDef.Create("USpeaker")
+            .FromAssembly(Events.Hooks.AssemblyCSharp)
+            .DerivesFrom<MonoBehaviour>()
+            .ConstructorCount(2)
+            .WithMethods(
+                MethodDef.Create().WithName("Awake"),
+                MethodDef.Create().WithName("LateUpdate"),
+                MethodDef.Create().WithName("OnDestroy"),
+                MethodDef.Create().WithName("Start"),
+                MethodDef.Create().WithName("Update")
+            )
+            .Setup()
+            .Resolved;
+
+        static USpeaker() => Type.LogAs(nameof(USpeaker));
+    }
+}
