35b4a921300da22aebf8f9781c29ade22608abf2
xKiraiChan <xKiraiChan@gmail.com>
2022-05-05 00:37:56 -0400

    add keybind attribute

---
+++ ModuleAPI/KeybindAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine.InputSystem;
+
+namespace KiraiMod.Core.ModuleAPI
+{
+    // todo: support boolean fields/props?
+    [AttributeUsage(AttributeTargets.Method)]
+    public class KeybindAttribute : InteractAttribute
+    {
+        private readonly Key[] Keys;
+
+        public KeybindAttribute(string Name, params Key[] Keys) : this(null, Name, Keys) {}
+        public KeybindAttribute(string Section, string Name, params Key[] Keys) : base(Section, Name + "Keybind") => this.Keys = Keys;
+
+        public override void Setup(Type Type, MemberInfo minfo)
+        {
+            base.Setup(Type, minfo);
+            Plugin.Configuration.Bind(Section, Name, Keys).Register(Invoke);
+        }
+    }
+}
