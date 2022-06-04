5cdaff63993be0b4017737bd1916340c05dd096e
xKiraiChan <xKiraiChan@gmail.com>
2022-06-03 22:12:51 -0400

    add error tolerance to VRCPlayer type

--- Types/VRCPlayer.cs
+++ Types/VRCPlayer.cs
@@ -7,11 +7,13 @@ namespace KiraiMod.Core.Types
 {
     public static class VRCPlayer
     {
-        public static readonly Type Type = Player.Type.GetProperties()
-            .First(x => x.PropertyType?.BaseType == typeof(Il2CppSystem.Object) && x.PropertyType.Name.EndsWith("Unique"))
-            .PropertyType;
+        public static readonly Type Type = Player.Type
+            ?.GetProperties()
+            .FirstOrDefault(x => x.PropertyType?.BaseType == typeof(Il2CppSystem.Object) && x.PropertyType.Name.EndsWith("Unique"))
+            ?.PropertyType;
 
-        public static readonly PropertyInfo m_GetHashtable = Type.GetProperties()
+        public static readonly PropertyInfo m_GetHashtable = Type
+            ?.GetProperties()
             .FirstOrDefault(x => x.PropertyType == typeof(Hashtable));
 
         static VRCPlayer()
