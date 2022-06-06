1401c970aaafa21da0ef027d6eafbb4947a9dde5
xKiraiChan <xKiraiChan@gmail.com>
2022-06-06 02:13:16 -0400

    add activation listener mono

---
+++ Utils/ActivationListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnhollowerBaseLib.Attributes;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+
+namespace KiraiMod.Core.Utils
+{
+    public class ActivationListener : MonoBehaviour
+    {
+        static ActivationListener() => ClassInjector.RegisterTypeInIl2Cpp<ActivationListener>();
+
+        [HideFromIl2Cpp] 
+        public event Action Enabled;
+
+        [HideFromIl2Cpp] 
+        public event Action Disabled;
+
+        public ActivationListener(IntPtr ptr) : base(ptr) { }
+
+        public void OnEnable() => Enabled?.StableInvoke();
+        public void OnDisable() => Disabled?.StableInvoke();
+    }
+}
